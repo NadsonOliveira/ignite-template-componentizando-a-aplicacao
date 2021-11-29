51acbc5fa0ce484b45765c413a0d8be47e562184
Nadson Oliveira <nsoliveira_13@hotmail.com>
2021-11-29 10:51:03 -0300

    Desafio rocketseat

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,49 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+import "./styles/content.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
 export function Content() {
-  // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </div>
+  );
 }
